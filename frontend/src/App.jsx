cd810e059abf819132d2e770070163da7c75ad5b
dbizzotto <dbizzotto@unaerp.br>
2024-05-29 18:56:50 -0300

    Feat: Remover fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
 
 function App() {
-
-  fetch('http://localhost:8080/atividades')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
   return (
     <p>Hello World!</p>
   )
